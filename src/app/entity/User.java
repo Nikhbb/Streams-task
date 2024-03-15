de4e2da07d08921e6dcf9c9a35617279f5a6a54a
vnovikov <nik.hbb@gmail.com>
2024-03-15 08:28:01 +0200

    Add user entity

---
+++ src/app/entity/User.java
@@ -0,0 +1,21 @@
+package app.entity;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name +
+                ", email: " + email;
+    }
+}
